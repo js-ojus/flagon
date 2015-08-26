1ff36f50a03fc4bbdb58f6b5ae46e59179ca5fce
JONNALAGADDA Srinivas <js@ojuslabs.com>
2015-08-26 22:45:32 +0530

    Add field types for `time.Time` and `string`

--- field.go
+++ field.go
@@ -18,6 +18,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+	"time"
 )
 
 // FieldType enumerates the possible data types of fields that can be
@@ -39,7 +40,6 @@ const (
 	FieldTypeFloat64
 	FieldTypeTime
 	FieldTypeString
-	FieldTypeBytes
 	FieldTypeReference
 	FieldTypeLink
 	FieldTypeCollection
@@ -62,7 +62,6 @@ func IsValidFieldType(t FieldType) bool {
 		FieldTypeFloat64,
 		FieldTypeTime,
 		FieldTypeString,
-		FieldTypeBytes,
 		FieldTypeReference, // strong reference
 		FieldTypeLink,      // weak reference
 		FieldTypeCollection:
@@ -126,7 +125,7 @@ func (f *FieldBool) Get() bool {
 	return false
 }
 
-// Set sets the given value to this field's storage.
+// Set sets the given value in this field's storage.
 func (f *FieldBool) Set(v bool) {
 	if v {
 		f.value = 1
@@ -155,7 +154,7 @@ func (f *FieldBool) WriteTo(w io.Writer) (int64, error) {
 	return 1, nil
 }
 
-// FieldInt8 represents a boolean value.
+// FieldInt8 represents an 8-bit integer value.
 type FieldInt8 struct {
 	basicField
 	value int8
@@ -166,7 +165,7 @@ func (f *FieldInt8) Get() int8 {
 	return f.value
 }
 
-// Set sets the given value to this field's storage.
+// Set sets the given value in this field's storage.
 func (f *FieldInt8) Set(v int8) {
 	f.value = v
 }
@@ -191,7 +190,7 @@ func (f *FieldInt8) WriteTo(w io.Writer) (int64, error) {
 	return 1, nil
 }
 
-// FieldInt16 represents a boolean value.
+// FieldInt16 represents a 16-bit integer value.
 type FieldInt16 struct {
 	basicField
 	value int16
@@ -202,7 +201,7 @@ func (f *FieldInt16) Get() int16 {
 	return f.value
 }
 
-// Set sets the given value to this field's storage.
+// Set sets the given value in this field's storage.
 func (f *FieldInt16) Set(v int16) {
 	f.value = v
 }
@@ -227,7 +226,7 @@ func (f *FieldInt16) WriteTo(w io.Writer) (int64, error) {
 	return 2, nil
 }
 
-// FieldInt32 represents a boolean value.
+// FieldInt32 represents a 32-bit integer value.
 type FieldInt32 struct {
 	basicField
 	value int32
@@ -238,7 +237,7 @@ func (f *FieldInt32) Get() int32 {
 	return f.value
 }
 
-// Set sets the given value to this field's storage.
+// Set sets the given value in this field's storage.
 func (f *FieldInt32) Set(v int32) {
 	f.value = v
 }
@@ -263,7 +262,7 @@ func (f *FieldInt32) WriteTo(w io.Writer) (int64, error) {
 	return 4, nil
 }
 
-// FieldInt64 represents a boolean value.
+// FieldInt64 represents a 64-bit integer value.
 type FieldInt64 struct {
 	basicField
 	value int64
@@ -274,7 +273,7 @@ func (f *FieldInt64) Get() int64 {
 	return f.value
 }
 
-// Set sets the given value to this field's storage.
+// Set sets the given value in this field's storage.
 func (f *FieldInt64) Set(v int64) {
 	f.value = v
 }
@@ -299,7 +298,7 @@ func (f *FieldInt64) WriteTo(w io.Writer) (int64, error) {
 	return 8, nil
 }
 
-// FieldUint8 represents a boolean value.
+// FieldUint8 represents an unsigned 8-bit integer value.
 type FieldUint8 struct {
 	basicField
 	value uint8
@@ -310,7 +309,7 @@ func (f *FieldUint8) Get() uint8 {
 	return f.value
 }
 
-// Set sets the given value to this field's storage.
+// Set sets the given value in this field's storage.
 func (f *FieldUint8) Set(v uint8) {
 	f.value = v
 }
@@ -335,7 +334,7 @@ func (f *FieldUint8) WriteTo(w io.Writer) (int64, error) {
 	return 1, nil
 }
 
-// FieldUint16 represents a boolean value.
+// FieldUint16 represents an unsigned 16-bit integer value.
 type FieldUint16 struct {
 	basicField
 	value uint16
@@ -346,7 +345,7 @@ func (f *FieldUint16) Get() uint16 {
 	return f.value
 }
 
-// Set sets the given value to this field's storage.
+// Set sets the given value in this field's storage.
 func (f *FieldUint16) Set(v uint16) {
 	f.value = v
 }
@@ -371,7 +370,7 @@ func (f *FieldUint16) WriteTo(w io.Writer) (int64, error) {
 	return 2, nil
 }
 
-// FieldUint32 represents a boolean value.
+// FieldUint32 represents an unsigned 32-bit integer value.
 type FieldUint32 struct {
 	basicField
 	value uint32
@@ -382,7 +381,7 @@ func (f *FieldUint32) Get() uint32 {
 	return f.value
 }
 
-// Set sets the given value to this field's storage.
+// Set sets the given value in this field's storage.
 func (f *FieldUint32) Set(v uint32) {
 	f.value = v
 }
@@ -407,7 +406,7 @@ func (f *FieldUint32) WriteTo(w io.Writer) (int64, error) {
 	return 4, nil
 }
 
-// FieldUint64 represents a boolean value.
+// FieldUint64 represents an unsigned 64-bit integer value.
 type FieldUint64 struct {
 	basicField
 	value uint64
@@ -418,7 +417,7 @@ func (f *FieldUint64) Get() uint64 {
 	return f.value
 }
 
-// Set sets the given value to this field's storage.
+// Set sets the given value in this field's storage.
 func (f *FieldUint64) Set(v uint64) {
 	f.value = v
 }
@@ -443,7 +442,7 @@ func (f *FieldUint64) WriteTo(w io.Writer) (int64, error) {
 	return 8, nil
 }
 
-// FieldFloat32 represents a boolean value.
+// FieldFloat32 represents a 32-bit float value.
 type FieldFloat32 struct {
 	basicField
 	value float32
@@ -454,7 +453,7 @@ func (f *FieldFloat32) Get() float32 {
 	return f.value
 }
 
-// Set sets the given value to this field's storage.
+// Set sets the given value in this field's storage.
 func (f *FieldFloat32) Set(v float32) {
 	f.value = v
 }
@@ -479,7 +478,7 @@ func (f *FieldFloat32) WriteTo(w io.Writer) (int64, error) {
 	return 4, nil
 }
 
-// FieldFloat64 represents a boolean value.
+// FieldFloat64 represents a 64-bit float value.
 type FieldFloat64 struct {
 	basicField
 	value float64
@@ -490,7 +489,7 @@ func (f *FieldFloat64) Get() float64 {
 	return f.value
 }
 
-// Set sets the given value to this field's storage.
+// Set sets the given value in this field's storage.
 func (f *FieldFloat64) Set(v float64) {
 	f.value = v
 }
@@ -513,4 +512,113 @@ func (f *FieldFloat64) WriteTo(w io.Writer) (int64, error) {
 	}
 
 	return 8, nil
+}
+
+// FieldTime represents a time value.
+type FieldTime struct {
+	basicField
+	value time.Time
+}
+
+// Get answers this field's value.
+func (f *FieldTime) Get() time.Time {
+	return f.value
+}
+
+// Set sets the given value in this field's storage.
+func (f *FieldTime) Set(v time.Time) {
+	f.value = v
+}
+
+// ReadFrom conforms to `io.ReaderFrom`.
+func (f *FieldTime) ReadFrom(r io.Reader) (int64, error) {
+	by := make([]byte, 0, 15)
+	n, err := r.Read(by)
+	if n != 15 && err != nil {
+		return int64(n), err
+	}
+
+	err = f.value.UnmarshalBinary(by)
+	if err != nil {
+		return 0, err
+	}
+
+	return 15, nil
+}
+
+// WriteTo conforms to `io.WriterTo`.
+func (f *FieldTime) WriteTo(w io.Writer) (int64, error) {
+	by, err := f.value.MarshalBinary()
+	if err != nil {
+		return 0, err
+	}
+
+	n, err := w.Write(by)
+	if n != 15 && err != nil {
+		return int64(n), err
+	}
+
+	return 15, nil
+}
+
+// FieldString represents a string value.
+type FieldString struct {
+	basicField
+	value string
+}
+
+// Get answers this field's value.
+func (f *FieldString) Get() string {
+	return f.value
+}
+
+// Set sets the given value in this field's storage.
+func (f *FieldString) Set(v string) {
+	if v == "" {
+		return
+	}
+
+	f.value = v
+}
+
+// ReadFrom conforms to `io.ReaderFrom`.
+func (f *FieldString) ReadFrom(r io.Reader) (int64, error) {
+	l := uint32(len(f.value))
+	err := binary.Read(r, binary.BigEndian, &l)
+	if err != nil {
+		return 0, err
+	}
+	if l == 0 {
+		return 0, io.EOF
+	}
+
+	by := make([]byte, 0, l)
+	n, err := r.Read(by)
+	if err != nil || n != int(l) {
+		return int64(n), err
+	}
+
+	f.value = string(by)
+	return int64(n), nil
+}
+
+// WriteTo conforms to `io.WriterTo`.
+func (f *FieldString) WriteTo(w io.Writer) (int64, error) {
+	if f.value == "" {
+		return 0, nil
+	}
+
+	by := []byte(f.value)
+	l := uint32(len(by))
+	err := binary.Write(w, binary.BigEndian, l)
+	if err != nil {
+		return 0, err
+	}
+
+	n, err := w.Write(by)
+	if err != nil || n != int(l) {
+		return int64(n), err
+	}
+
+	return int64(n), nil
 }
